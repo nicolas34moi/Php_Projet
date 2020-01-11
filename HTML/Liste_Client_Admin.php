01e17bcd4e6a1d1049aa4acf94411051e032347b
GR <guillaume.ros@epsi.fr>
2020-01-11 16:17:04 +0100

    Modif Liste Client

---
+++ HTML/Liste_Client_Admin.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <link rel="stylesheet" href="tableau.css">
+</head>
+<body>
+
+<header>
+    <h1>Hôtel Neptune</h1>
+</header>
+
+<form action="" method="post" id="Recherche">
+    <label for="">Magasin : </label>
+    <input type="text" name="noChambre" value="">
+    <input type="submit" value="Rechercher" />
+</form>
+
+<table>
+    <thead>
+    <tr>
+        <th>Nom</th>
+        <th>Prénom</th>
+        <th>N° de Téléphone</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Bouchar</td>
+        <td>Gérard</td>
+        <td>0616263646</td>
+        <td>Modifier</td>
+    </tr>
+
+    </tbody>
+</table>
+
+
+
+
+</body>
+</html>
