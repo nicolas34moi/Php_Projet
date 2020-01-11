827b016114c0bb4996d362c963da026f218cf323
GR <guillaume.ros@epsi.fr>
2020-01-11 16:04:43 +0100

    1er jet

--- HTML/Liste_Reserv_Admin.php
+++ HTML/Liste_Reserv_Admin.php
@@ -1,7 +1,37 @@
 <html>
 <body>
-<h1>Salut C'est moi</h1>
+<link rel="stylesheet" href="tableau.css">
+
+
+<form action="" method="post">
+    <label for="">Magasin : </label>
+    <select name="stor_id">
+        <option value=''>Tous</option>
+    </select>
+    <input type="submit" value="Rechercher" />
+</form>
+
+<table>
+    <thead>
+    <tr>
+        <th>N° Chambre</th>
+        <th>Date de Début</th>
+        <th>Date de Fin</th>
+    </tr>
+    </thead>
+    <tbody>
+             <tr>
+                 <td>16</td>
+                 <td>11/01/2020</td>
+                 <td>18/01/2020</td>
+             </tr>
+
+
+    </tbody>
+</table>
+
+
+
+
 </body>
 </html>
-<?php
-?>
