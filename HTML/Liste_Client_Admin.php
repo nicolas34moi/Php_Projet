82fd0cdbe26a7594adab3738b4128bd0ba7dfe83
GR <guillaume.ros@epsi.fr>
2020-01-13 16:34:53 +0100

    bootstrap

--- HTML/Liste_Client_Admin.php
+++ HTML/Liste_Client_Admin.php
@@ -2,6 +2,17 @@
 <html>
 <head>
     <link rel="stylesheet" href="tableau.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -15,42 +26,42 @@
     <input type="submit" value="Rechercher" />
 </form>
 
-<table>
-    <thead>
+<table  class="table table-striped">
+    <thead class="thead-dark">
     <tr>
-        <th>Nom</th>
-        <th>Prénom</th>
-        <th>N° de Téléphone</th>
-        <th></th>
+        <th scope="col">Nom</th>
+        <th scope="col">Prénom</th>
+        <th scope="col">N° de Téléphone</th>
+        <th scope="col"></th>
     </tr>
     </thead>
     <tbody>
     <tr>
-        <td>Bouchar</td>
+        <td scope="row">Bouchar</td>
         <td>Gérard</td>
         <td>0616263646</td>
         <td><a href="Modif_Client_Admin.php">Modifier</a></td>
     </tr>
     <tr>
-        <td>Mendigot</td>
+        <td scope="row">Mendigot</td>
         <td>Gueusar</td>
         <td>0615253545</td>
         <td><a href="Modif_Client_Admin.php">Modifier</a></td>
     </tr>
     <tr>
-        <td>Quomunyc</td>
+        <td scope="row">Quomunyc</td>
         <td>Dominique</td>
         <td>0614243444</td>
         <td><a href="Modif_Client_Admin.php">Modifier</a></td>
     </tr>
     <tr>
-        <td>Kipyk</td>
+        <td scope="row">Kipyk</td>
         <td>Clovis</td>
         <td>0717273747</td>
         <td><a href="Modif_Client_Admin.php">Modifier</a></td>
     </tr>
     <tr>
-        <td>Tournevis</td>
+        <td scope="row">Tournevis</td>
         <td>Sonic</td>
         <td>07700 900461</td>
         <td><a href="Modif_Client_Admin.php">Modifier</a></td>
--- HTML/Liste_Reserv_Admin.php
+++ HTML/Liste_Reserv_Admin.php
@@ -1,6 +1,17 @@
 <html>
 <head>
     <link rel="stylesheet" href="tableau.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -11,11 +22,11 @@
 <form action="" method="post" id="Recherche">
     <label for="">N° Chambre : </label>
     <input type="text" name="noChambre" value="">
-    <input type="submit" value="Rechercher" />
+    <input type="submit" value="Rechercher"/>
 </form>
 
-<table>
-    <thead>
+<table  class="table table-striped">
+    <thead class="thead-dark">
     <tr>
         <th>N° Chambre</th>
         <th>Date de Début</th>
@@ -23,41 +34,39 @@
     </tr>
     </thead>
     <tbody>
-             <tr>
-                 <td>12</td>
-                 <td>11/01/2020</td>
-                 <td>18/01/2020</td>
-             </tr>
-             <tr>
-                 <td>9</td>
-                 <td>1/01/2020</td>
-                 <td>1/01/2020</td>
-             </tr>
-             <tr>
-                 <td>9</td>
-                 <td>1/01/2020</td>
-                 <td>1/01/2020</td>
-             </tr>
-             <tr>
-                 <td>9</td>
-                 <td>1/01/2020</td>
-                 <td>1/01/2020</td>
-             </tr>
-             <tr>
-                 <td>9</td>
-                 <td>1/01/2020</td>
-                 <td>1/01/2020</td>
-             </tr>
-             <tr>
-                 <td>9</td>
-                 <td>1/01/2020</td>
-                 <td>1/01/2020</td>
-             </tr>
+    <tr>
+        <td>12</td>
+        <td>11/01/2020</td>
+        <td>18/01/2020</td>
+    </tr>
+    <tr>
+        <td>9</td>
+        <td>1/01/2020</td>
+        <td>1/01/2020</td>
+    </tr>
+    <tr>
+        <td>9</td>
+        <td>1/01/2020</td>
+        <td>1/01/2020</td>
+    </tr>
+    <tr>
+        <td>9</td>
+        <td>1/01/2020</td>
+        <td>1/01/2020</td>
+    </tr>
+    <tr>
+        <td>9</td>
+        <td>1/01/2020</td>
+        <td>1/01/2020</td>
+    </tr>
+    <tr>
+        <td>9</td>
+        <td>1/01/2020</td>
+        <td>1/01/2020</td>
+    </tr>
 
     </tbody>
 </table>
-
-
 
 
 </body>
